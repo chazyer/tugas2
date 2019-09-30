08e3178828718e536aa2ea4975a60c9229e539c2
chazyer <charliegnw06@gmail.com>
2019-10-01 01:08:08 +0700

    tugas 2

---
+++ app/src/main/java/android/example/order/MainActivity.java
@@ -0,0 +1,96 @@
+package android.example.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TextView quantity;
+    public CheckBox whippedCream;
+    public CheckBox chocolate;
+    public EditText name;
+    public String isWhippedCream;
+    public String isChocolate;
+    public String userName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        quantity = (TextView)findViewById(R.id.quantity_text_view);
+        whippedCream = (CheckBox)findViewById(R.id.whipped_cream);
+        chocolate = (CheckBox)findViewById(R.id.chocolate);
+        name = (EditText)findViewById(R.id.name);
+    }
+
+    public void increment(View view){
+        String s = quantity.getText().toString();
+        int quant = Integer.parseInt(s,10);
+        quant++;
+        quantity.setText(""+quant);
+    }
+
+    public void decrement(View view){
+        String s = quantity.getText().toString();
+        int quant = Integer.parseInt(s,10);
+        quant--;
+        quantity.setText(""+quant);
+    }
+
+    public void submitOrder(View view){
+        String s = quantity.getText().toString();
+        int quant = Integer.parseInt(s);
+        if(whippedCream.isChecked()){
+            isWhippedCream = "Yes";
+        }
+        else{
+            isWhippedCream = "No";
+        }
+        if(chocolate.isChecked()){
+            isChocolate = "Yes";
+        }
+        else{
+            isChocolate = "No";
+        }
+        int price = getPrice(quant);
+        userName = name.getText().toString();
+        String orderSummary = getOrderSummary(price,quant);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee order for " + userName);
+        intent.putExtra(Intent.EXTRA_TEXT, orderSummary);
+        intent.putExtra(Intent.EXTRA_EMAIL, "shivam.vk529@gmail.com");
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+        Toast.makeText(getApplicationContext(), "Order will be delivered in 5 mins ^_^",Toast.LENGTH_LONG).show();
+    }
+
+    public int getPrice(int quant){
+        if(whippedCream.isChecked() && chocolate.isChecked()){
+            return 8*quant;
+        }
+        else if(whippedCream.isChecked() && !chocolate.isChecked()){
+            return 6*quant;
+        }
+        else if(!whippedCream.isChecked() && chocolate.isChecked()){
+            return 7*quant;
+        }
+        else
+            return 5*quant;
+    }
+
+    public String getOrderSummary(int price, int quant){
+        String orderSummary = "Name: " + userName + "\nAdd Whipped Cream? " + isWhippedCream + "\nAdd Chocolate?" + isChocolate + "\nQuantity: " + quant + "\nTotal: $" + price + "\nThankyou!";
+        return orderSummary;
+    }
+}
